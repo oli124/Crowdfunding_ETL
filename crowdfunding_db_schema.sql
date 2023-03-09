810e149cac3623a3f16621867a0d79d225edd8dc
oli124 <ogwking@icloud.com>
2023-03-09 15:54:26 +0800

    updating notebook and db schema files

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,33 @@
+contacts
+--------
+contact_id INT PK FK - campaign.contact_id
+first_name VARCHAR(50)
+last_name VARCHAR(50)
+email VARCHAR(100)
+
+campaign
+--------
+cf_id INT
+contact_id INT
+company_name VARCHAR(100)
+description VARCHAR(100)
+goal INT
+pledged INT
+outcome VARCHAR(50)
+backers_count VARCHAR(50)
+country VARCHAR(50)
+currency VARCHAR(50)
+launch_date DATE
+end_date DATE
+category_id VARCHAR(50) FK
+subcategory_id VARCHAR(50) FK
+
+category
+--------
+category_id VARCHAR(50) PK FK -< campaign.category_id
+category VARCHAR(50)
+
+subcategory
+--------
+subcategory_id VARCHAR(50) PK FK -< campaign.subcategory_id
+subcategory VARCHAR(50)
